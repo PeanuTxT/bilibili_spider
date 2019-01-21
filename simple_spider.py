96e46ab68434470ac39b42a3e38da511849df908
Peanut <568112825@qq.com>
2019-01-21 11:42:48 +0800

    简易爬虫小程序

--- simple_spider.py
+++ simple_spider.py
@@ -1,11 +1,19 @@
 # -*- coding:utf-8 -*-
 
 import requests
+import time
 import re
 
 
+'''
+函数功能：获取弹幕文件的xml地址
+参数：
+    av_num：视频av号
+返回：
+    无
+'''
 def get_xml_file(av_num):
-    url = 'https://www.bilibili.com/video/av' + str(av_num)
+    url = 'https://www.bilibili.com/video/av' + av_num
     headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063' }
     html_code = requests.get(url, headers=headers).text
 
@@ -14,14 +22,47 @@ def get_xml_file(av_num):
         pass
     cid_num = re.match(f'.*"cid=(\d+)&aid={av_num}',html_code).group(1)
     xml_url = 'http://comment.bilibili.com/' + cid_num + '.xml'
-    xml_data = requests.get(xml_url, headers=headers).content
+    xml_data = str(requests.get(xml_url, headers=headers).content, encoding = "utf-8")
 
-    # 弹幕文件保存为csv
-    with open(str(av_num) + '.xml', 'wb') as f:
-        f.write(xml_data)
+    # 格式化xml文件
+    xml_format(av_num, xml_data)
 
-    print('done...')
+
+'''
+函数功能：格式化xml弹幕文件
+参数：
+    av_num：视频av号
+    xml_data：需要格式化的xml的字符串
+返回：
+    无
+'''
+def xml_format(av_num, xml_data):
+
+    # 正则匹配各级标签
+    head = re.match('(<\?xml.+?>)',xml_data).group(1)
+    tag_i = re.findall('</*i>',xml_data)
+    danmu_list = re.findall('<.*?>.*?</.*?>',xml_data)
+    body = ''
+
+    # 对标签进行换行和缩进
+    for i,tag in enumerate(danmu_list):
+        if i == 0:
+            body += '\n\t' + tag.replace(head + tag_i[0], '') + '\n\t'
+        elif i == len(danmu_list) - 1:
+            body += tag + '\n'
+        else:
+            body += tag + '\n\t'
+
+    # 输出格式化后的结果
+    result = head + '\n' + tag_i[0] + body + tag_i[1]
+    with open(f'{av_num}.xml', 'w', encoding = "utf-8") as f:
+        f.write(result)
+
 
 if __name__ == '__main__':
-    get_xml_file('40681810')
+    input_num = input('输入视频av号：\n')
+    get_xml_file(input_num)
+    print(u'弹幕爬取完成...')
+    print(u'2秒后自动退出程序...')
+    time.sleep(2)
 
